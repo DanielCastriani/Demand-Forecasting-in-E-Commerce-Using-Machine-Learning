426a5073c23b3011f8c30dcfb33d0532922eecd7
DanielCastriani <dcastriani@gmail.com>
2021-07-15 22:08:42 -0300

    #2 - transform and merge data

--- import_data/transform_data.py
+++ import_data/transform_data.py
@@ -1,17 +1,15 @@
-from utils.feature_engineering_utils import group_order_items
-from schemas import order_item_schema, order_schema, customer_schema, product_schema, seller_schema
 from pyspark import SparkContext
 from pyspark.sql import SparkSession
 
+from schemas import customer_schema, order_item_schema, order_schema, product_schema, seller_schema
+from utils.feature_engineering_utils import calc_order_data_feature, filter_order, group_order_items
 
 master = 'spark://127.0.1.1:7077'
 appName = 'Transform Data'
+file_path = 'hdfs://localhost:9000/user/daniel/dataset/{}'
 
 sc = SparkContext(master=master, appName=appName)
 spark = SparkSession.builder.appName(appName).master(master).getOrCreate()
-
-
-file_path = 'hdfs://localhost:9000/user/daniel/dataset/{}'
 
 
 customers = spark.read.csv(file_path.format('olist_customers_dataset.csv'), header=True, schema=customer_schema)
@@ -21,3 +19,13 @@ products = spark.read.csv(file_path.format('olist_products_dataset.csv'), header
 sellers = spark.read.csv(file_path.format('olist_sellers_dataset.csv'), header=True, schema=seller_schema)
 
 order_items = group_order_items(order_items)
+
+orders = filter_order(orders)
+orders = calc_order_data_feature(orders)
+
+dataset = orders.join(order_items, on=['order_id'])
+dataset = dataset.join(products, on=['product_id'])
+dataset = dataset.join(customers, on=['customer_id'])
+dataset = dataset.join(sellers, on=['seller_id'])
+
+dataset.limit(5).toPandas().head()
--- import_data/utils/feature_engineering_utils.py
+++ import_data/utils/feature_engineering_utils.py
@@ -1,5 +1,6 @@
 from pyspark.sql import DataFrame
 from pyspark.sql import functions as F
+from pyspark.sql.types import IntegerType
 
 
 def group_order_items(order_items: DataFrame):
@@ -20,3 +21,31 @@ def group_order_items(order_items: DataFrame):
     order_items = order_items.withColumn('total', order_items.price + order_items.freight_value)
 
     return order_items
+
+
+def calc_order_data_feature(orders: DataFrame):
+    orders = orders.withColumnRenamed('order_purchase_timestamp', 'date')
+
+    orders = orders\
+        .withColumn('days_to_approve', F.datediff(orders.order_approved_at, orders.date))\
+        .withColumn('days_to_post', F.datediff(orders.order_delivered_carrier_date, orders.date))\
+        .withColumn('days_to_deliver', F.datediff(orders.order_delivered_customer_date, orders.date))\
+        .withColumn('days_estimated_to_deliver', F.datediff(orders.order_estimated_delivery_date, orders.date))
+
+    orders = orders.withColumn('estimated_delivered_diff', orders.days_estimated_to_deliver - orders.days_to_deliver)
+
+    orders = orders.withColumn('is_delayed', orders.estimated_delivered_diff < 0)
+
+    orders = orders.drop(
+        'order_approved_at',
+        'order_delivered_carrier_date',
+        'order_delivered_customer_date',
+        'order_estimated_delivery_date',
+    )
+
+    return orders
+
+
+def filter_order(orders: DataFrame):
+    orders = orders.where(~orders.order_status.isin(['canceled', 'unavailable']))
+    return orders
